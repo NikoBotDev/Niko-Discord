6c369272cc1553aff161816b6e7b9d3502786668
NikoBotDev <joao_vitor9987@outlook.com.br>
2019-10-19 18:37:19 -0300

    Register new routes

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const logger = require('../classes/Logger');
 const actionsRouter = require('./routes/actions');
+const usersRouter = require('./routes/users');
+const guildUsersRouter = require('./routes/users/guild');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(actionsRouter);
-
+app.use('/actions', actionsRouter);
+app.use('/users', [usersRouter, guildUsersRouter]);
 app.get('/', (_, res) => {
   res.status(200).send('Hello');
 });
